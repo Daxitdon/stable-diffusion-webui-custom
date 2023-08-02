216247392e7a0a40b0806818b7e75659ff7d808f
daxit <daxitm2112@gmail.com>
2023-08-02 11:45:53 +0530

    remove loop

--- modules/sd_models.py
+++ modules/sd_models.py
@@ -195,13 +195,9 @@ def select_checkpoint():
 
     model_list = modelloader.load_models(model_path=model_path, model_url=model_url, command_path=shared.cmd_opts.ckpt_dir, ext_filter=[".ckpt", ".safetensors"], ext_blacklist=[".vae.ckpt", ".vae.safetensors"])
     print(model_list)
-    for filename in sorted(model_list, key=str.lower):
-        print("filename",filename)
-        print("name", name)
-        if filename == name:
-            print("Filename == name")
-            checkpoint_info = CheckpointInfo(filename)
-            checkpoint_info.register()
+
+    checkpoint_info = CheckpointInfo(name)
+
     #
     # checkpoint_info = checkpoint_aliases.get(model_checkpoint, None)
     # if checkpoint_info is not None:
